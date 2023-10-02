5fe9363fcdbc831d1719e5d0514536d6bdedd5ec
heksaani <heksaani@Heidis-MacBook-Pro.local>
2023-10-02 14:39:58 +0300

    Add exercise folder and from week 2 ex 1 and 2

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,7 @@
 class RatingsController < ApplicationController
     def index # this will render the index.html.erb file in the views/ratings folder
-        #@ratings = Rating.all
+        @ratings = Rating.all
     end
+    # get all ratings for a poem
+    #def 
 end
